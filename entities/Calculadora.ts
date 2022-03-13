e70da4892696bee7dad19c68507ff5943c3ed7dc
Carlos Germano <carlos.germano@cmtecnologia.com.br>
2022-03-13 15:40:02 -0300

    Updates

---
+++ entities/Calculadora.ts
@@ -0,0 +1,23 @@
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+export function subtract(a: number, b: number) {
+    return a - b;
+}
+
+export function multiple(a: number, b: number) {
+    return a * b;
+}
+
+export function division(a: number, b: number) {
+    return a / b;
+}
+
+export function percentage(a: number) {
+    return (a / 100);
+}
+
+export function potencyOfTwo(a: number) {
+    return (a * a);
+}
