41a02fb8fafc0bd8b8362196f44fc2aab06f7408
Carlos Germano Carvalho <carlos.germano@cmtecnologia.com.br>
2022-03-16 22:02:16 -0300

    üpdates

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { sum, history, multiple } from './entities/Calculadora'
 
 
-console.log(history([{param1: 1, param2: 2}], ['sum']));
-console.log(history([{param1: 1, param2: 2}], ['subtract']));
-console.log(history([{param1: 1, param2: 2}], ['multiple']));
-console.log(history([{param1: 2, param2: 3}], ['division']));
-console.log(history([{param1: 60}], ['percentage']));
-console.log(history([{param1: 2}], ['potencyOfTwo']));
-console.log(history([{param1: 3, param2: 2}, {param1: sum(3, 2), param2: 6}, {param1: multiple(sum(3, 2), 6), param2: 6}], ['sum', 'multiple', 'potencyOfTwo']))
+console.log(history([{ param1: 1, param2: 2 }], ['sum']));
+console.log(history([{ param1: 1, param2: 2 }], ['subtract']));
+console.log(history([{ param1: 1, param2: 2 }], ['multiple']));
+console.log(history([{ param1: 2, param2: 3 }], ['division']));
+console.log(history([{ param1: 60 }], ['percentage']));
+console.log(history([{ param1: 2 }], ['potencyOfTwo']));
+console.log(history([{ param1: 3, param2: 2 }, { param1: sum(3, 2), param2: 6 }, { param1: multiple(sum(3, 2), 6), param2: 6 }], ['sum', 'multiple', 'potencyOfTwo']))
